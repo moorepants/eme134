f072401a72909d49e8f6e33fe158572aa60c51e5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-04-01 04:00:27 +0000

    Update docs after building Travis build 82 of moorepants/eme134
    The docs were built from the branch 'master' against the commit
    35f935cffa014b6abecd64425d3221a2af115e3e.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/moorepants/eme134/jobs/669533848.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --build-tags --built-docs output/ .

---
+++ scripts/eval_train_wheelset_outputs.m
@@ -0,0 +1,29 @@
+function y = eval_train_wheelset_outputs(t, x, c)
+% EVAL_TRAIN_WHEELSET_OUTPUTS - Returns the output vector at the specified
+% time.
+%
+% Syntax: y = eval_quarter_car_outputs(t, x, c)
+%
+% Inputs:
+%   t - Scalar value of time, size 1x1.
+%   x - State vector at time t, size 4x1, [q1; q2; u1; u2]
+%   c - Constant parameter structure with 16 parameters.
+% Outputs:
+%   y - Output vector at time t, size 2x1, [Fx; Fy].
+
+% unpack xdot and x as needed
+
+replace this line with your lines
+
+% unpack the parameters
+
+replace this line with your lines
+
+% calculate the forces
+
+replace this line with your line(s)
+
+% pack the outputs into a qx1 vector
+y = [Fx; Fy];
+
+end
